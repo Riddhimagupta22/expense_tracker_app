024820a6a43364684551349b231585ca898621d6
Riddhima Gupta <griddhima226@gmail.com>
2025-06-21 12:32:27 +0530

    new project

--- lib/Modules/Profile/view/profile.dart
+++ lib/Modules/Profile/view/profile.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker_app/Comman_Wigets/widget/custom%20Container/custom_container.dart';
 import 'package:expense_tracker_app/Modules/Profile/view/widgets/cricular_widget.dart';
+import 'package:expense_tracker_app/Modules/Profile/view/widgets/profile_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -13,58 +14,90 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Stack(
-        children: [
-          Column(
-            children: [
-              CustomContainer(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                CustomContainer(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 60),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
                             onPressed: () {
                               Get.to(NavBar());
                             },
-                            icon: Icon(
+                            icon: const Icon(
                               Icons.arrow_back_ios_new_outlined,
                               size: 20,
                               color: Colors.white,
-                            )),
-                        Text(
-                          "Profile",
-                          style: GoogleFonts.inter(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
+                            ),
                           ),
-                        ),
-                        IconButton(
+                          Text(
+                            "Profile",
+                            style: GoogleFonts.inter(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          IconButton(
                             onPressed: () {},
-                            icon: Icon(
+                            icon: const Icon(
                               Icons.more_horiz,
                               size: 28,
                               color: Colors.white,
-                            )),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 100), // Space for profile image
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                    child: ListView(
+                      children: const [
+                        SizedBox(height: 50), // Space below image
+                        ProfileMenu(title: "Invite Friends", icon: Icons.person_add_alt_1),
+                        ProfileMenu(title: "Account Info", icon: Icons.account_circle),
+                        ProfileMenu(title: "Personal Profile", icon: Icons.person_outline),
+                        ProfileMenu(title: "Message Center", icon: Icons.message),
+                        ProfileMenu(title: "Login & Security", icon: Icons.security),
+                        ProfileMenu(title: "Data and Privacy", icon: Icons.lock),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Positioned(
+              ],
+            ),
+
+            // Profile Image Positioned on top
+            const Positioned(
               top: 200,
               left: 150,
-              child: const CircularImage(
+              child: CircularImage(
                 image: 'Assets/Image/hand-drawn-ethnic-beauty-illustration.png',
-              ))
-        ],
-      )),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/Modules/Profile/view/widgets/profile_menu.dart
+++ lib/Modules/Profile/view/widgets/profile_menu.dart
@@ -1,51 +1,19 @@
 import 'package:flutter/material.dart';
-
-class GProfileMenu extends StatelessWidget {
+import 'package:google_fonts/google_fonts.dart';
+class ProfileMenu extends StatelessWidget {
   final String title;
-  final void Function()? onPressed;
-  final Color? bgcolor;
-  final Color? iconcolor;
   final IconData icon;
 
-  const GProfileMenu({
-    super.key,
-    required this.title,
-    this.onPressed,
-    required this.icon,
-    this.bgcolor,
-    this.iconcolor,
-  });
+  const ProfileMenu({super.key, required this.title, required this.icon});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPressed,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        child: Row(
-          children: [
-            CircleAvatar(
-              radius: 15,
-              backgroundColor: bgcolor ?? Colors.grey.shade300,
-              child: Icon(
-                icon,
-                size: 17,
-                color: iconcolor ?? Colors.black,
-              ),
-            ),
-            const SizedBox(width: 25),
-            Expanded(
-              flex: 6,
-              child: Text(
-                title,
-                style: Theme.of(context).textTheme.bodyMedium,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            const Icon(Icons.keyboard_arrow_right_rounded, size: 29),
-          ],
+    return ListTile(
+        leading: Icon(icon, color: Color.fromRGBO(102, 102, 102, 1)),
+
+        title: Text(
+          title,
+          style: GoogleFonts.inter(fontSize: 18, fontWeight: FontWeight.w500),
         ),
-      ),
-    );
-  }
-}
+        contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+        onTap: () {});}}
